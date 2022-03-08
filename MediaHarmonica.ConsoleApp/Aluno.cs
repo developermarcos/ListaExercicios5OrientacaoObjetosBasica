485599a23d524582d9df5ea1ecfe7e62dd5282f4
Marcos Lima <developermarcos@outlook.com>
2022-03-08 16:35:58 -0300

    Exercicio media harmonica finalizado

---
+++ MediaHarmonica.ConsoleApp/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaHarmonica.ConsoleApp
+{
+    internal class Aluno
+    {
+        public string nome;
+        public decimal nota1;
+        public decimal nota2;
+        public decimal nota3;
+        public decimal nota4;
+
+        public decimal CalcularMediaHarmonica()
+        {
+            decimal result = 4 / ((1/nota1) + (1/nota2) + (1/nota3) + (1/nota4));
+            return result;
+        }
+    }
+}
---
+++ MediaHarmonica.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MediaHarmonica.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Aluno aluno = new Aluno();
+            aluno.nome = "Marcos Lima";
+            aluno.nota1 = 9.00m;
+            aluno.nota2 = 7.00m;
+            aluno.nota3 = 6.00m;
+            aluno.nota4 = 8.00m;
+            Console.WriteLine("Aluno {0} teve a media harmonica de {1}", aluno.nome, aluno.CalcularMediaHarmonica().ToString("N2"));
+            Console.ReadKey();
+        }
+    }
+}
